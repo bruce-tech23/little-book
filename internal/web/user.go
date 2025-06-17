8540159089077d18f7b0e2d4a1711286455a0a4c
bruce-tech23 <bruce.tech@qq.com>
2025-06-17 17:53:08 +0800

    userinfo validate

--- internal/web/user.go
+++ internal/web/user.go
@@ -1,1 +1,83 @@
 package web
+
+import (
+	regexp "github.com/dlclark/regexp2" // 官方自带的 regexp 不支持 ?=.
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const (
+	emailRegexPattern = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"
+	// 和上面比起来，用 ` 看起来就比较清爽
+	passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$`
+)
+
+type UserHandler struct {
+	emailRegExp    *regexp.Regexp
+	passwordRegExp *regexp.Regexp
+}
+
+func NewUserHandler() *UserHandler {
+	return &UserHandler{
+		emailRegExp:    regexp.MustCompile(emailRegexPattern, regexp.None),
+		passwordRegExp: regexp.MustCompile(passwordRegexPattern, regexp.None),
+	}
+}
+
+func (h *UserHandler) RegisterRoutes(server *gin.Engine) {
+	ug := server.Group("/users")
+	ug.POST("/signup", h.SignUp)
+	ug.POST("/login", h.Login)
+	ug.POST("/edit", h.Edit)
+	ug.GET("/profile", h.Profile)
+}
+
+func (h *UserHandler) SignUp(ctx *gin.Context) {
+	type SignupReq struct {
+		Email           string `json:"email"`
+		Password        string `json:"password"`
+		ConfirmPassword string `json:"confirmPassword"`
+	}
+	var req SignupReq
+	if err := ctx.Bind(&req); err != nil {
+		return
+	}
+
+	isEmail, err := h.emailRegExp.MatchString(req.Email)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !isEmail {
+		ctx.String(http.StatusOK, "邮箱格式不正确")
+		return
+	}
+
+	if req.ConfirmPassword != req.Password {
+		ctx.String(http.StatusOK, "两次密码不匹配")
+		return
+	}
+
+	isPassword, err := h.passwordRegExp.MatchString(req.Password)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !isPassword {
+		ctx.String(http.StatusOK, "密码格式不正确")
+		return
+	}
+	ctx.String(http.StatusOK, "hello, 你在注册")
+}
+
+func (h *UserHandler) Login(ctx *gin.Context) {
+
+}
+
+func (h *UserHandler) Edit(ctx *gin.Context) {
+
+}
+
+func (h *UserHandler) Profile(ctx *gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"geektime-basic-learning2/little-book/internal/web"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func main() {
 	server := gin.Default()
+	// 跨域处理
+	server.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		// 旧版本AllowHeaders可以不写，默认的就行，新版本跨域时这个 Origin 必须要显示的写上
+		AllowHeaders: []string{"Content-Type"},
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+	hdl := web.NewUserHandler()
+	hdl.RegisterRoutes(server)
 	err := server.Run(":8080")
 	if err != nil {
 		panic("Server run failed.")
